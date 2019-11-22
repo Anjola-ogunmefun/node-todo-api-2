bd40ffb75091b5872c1bfb633d1e4866aea2e1cf
Anjola-ogunmefun <ogunmefunanjola@gmail.com>
2019-11-22 13:22:36 +0100

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -22,6 +22,14 @@ todo.save().then((doc) => {
 });
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
